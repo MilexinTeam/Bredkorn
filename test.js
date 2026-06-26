419e182bcbd25c3c3bb5c87f9e3e8c7dcec1fd40
MilexinTeam <fundatory.milexinteam@gmail.com>
2026-06-26 06:33:04 +0200

    [FIX] Fix test

--- test.js
+++ test.js
@@ -1,35 +1,21 @@
-let BredkornModule;
-
-try {
-  BredkornModule = await import("./dist/index.js");
-} catch {
-  BredkornModule = await import("./index.js");
-}
-
-// jeśli eksport domyślny, bierzemy go, inaczej cały moduł
-const Bredkorn = BredkornModule.default ?? BredkornModule;
-
-const editor = new Bredkorn(document.querySelector(".editor"), {
-  theme: "cs-dark-plus",
-});
-
-editor.addTheme("dht-bamboo-light", {
-  inherit: "cs-dark",
-  main: {
+import Bredkorn from "./dist/index.js";
+const editor = new Bredkorn(document.querySelector(".editor"), {theme: "cs-dark-plus"})
+editor.addTheme("dht-bamboo-light", 
+    {
+        inherit: "cs-dark",
+main: {
     bgColor: "#daf5bb",
     lineNumberBgColor: "#a7b992",
     lineBgColor: "#cee7b2",
-  },
-});
-
+}
+})
 editor.addTheme("dht-bamboo-dark", {
-  inherit: "cs-dark",
-  main: {
-    bgColor: "#0f140c",
-    lineNumberBgColor: "#1a2214",
-    lineBgColor: "#131a10",
-  },
-});
-
-await editor.init();
-editor.setTheme("cs-dark-plus");
+    inherit: "cs-dark",
+    main: {
+        bgColor: "#0f140c",          // bardzo ciemna zieleń (prawie czarna)
+        lineNumberBgColor: "#1a2214", // ciemny bambusowy panel
+        lineBgColor: "#131a10",       // delikatnie jaśniejsze tło linii
+    }
+})
+await editor.init()
+editor.setTheme("cs-dark-plus")
